b88744f3ee5f7df57019b3f08d758b35757eb85b
Simon Hasak <simon.hasak@gmail.com>
2020-03-20 12:39:10 +0100

    New modules added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EnterEventsComponent } from './enter-events/enter-events.component';
 
+import { EnterEventsComponent } from './enter-events/enter-events.component';
 import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {FormsModule} from '@angular/forms';
 import { IsTimeEventsComponent } from './is-time-events/is-time-events.component';
+import { NotifyServiceComponent } from './notify-service/notify-service.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EnterEventsComponent,
-    IsTimeEventsComponent
+    IsTimeEventsComponent,
+    NotifyServiceComponent
   ],
   imports: [
     BrowserModule,
